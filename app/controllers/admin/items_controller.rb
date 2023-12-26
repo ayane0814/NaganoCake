f8c69a5142c07f9d16c1a349ff8a8c77271bc0e8
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2023-12-26 12:51:06 +0000

    Add 管理者:商品新規登録、詳細、一覧、編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,6 +2,7 @@ class Admin::ItemsController < ApplicationController
     before_action :authenticate_admin!
     
     def index
+        @items = Item.page(params[:page])
     end
     
     def new
@@ -9,15 +10,28 @@ class Admin::ItemsController < ApplicationController
     end
     
     def create
+        @item = Item.new(item_params)
+        @item.save
+        redirect_to admin_item_path(@item.id)
     end
     
     def show
+        @item = Item.find(params[:id])
     end
     
     def edit
+        @item = Item.find(params[:id])
     end
     
     def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to admin_item_path(@item.id)
     end
     
+    private
+    
+    def item_params
+        params.require(:item).permit(:name, :introduction, :price, :item_image)
+    end
 end
