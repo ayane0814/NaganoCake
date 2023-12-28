dbc98ca958055edf57cf0087f73f34700b546ea0
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2023-12-28 12:27:43 +0000

    Add 管理者_会員管理

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def status_label
+    is_active? ? '有効' : '退会'
+  end
 end
