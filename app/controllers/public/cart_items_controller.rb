bebc5d19cf2b6332eb262d6a66fcf65e79ac6e76
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2023-12-29 09:46:12 +0000

    Add カート内の商品

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,9 +2,13 @@ class Public::CartItemsController < ApplicationController
     before_action :authenticate_customer!
     
     def index
+        @cart_items = CartItem.all
     end
     
     def update
+        cart_item = CartItem.find(params[:id])
+        cart_item.update(cart_item_params)
+        redirect_back(fallback_location: root_path)
     end
     
     def destroy
@@ -14,8 +18,18 @@ class Public::CartItemsController < ApplicationController
     end
     
     def create
-        binding.pry
-        @item = Item.find(cart_item_params[:item_id])
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.customer_id = current_customer.id
+        @cart_item.item_id = cart_item_params[:item_id]
+        if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+            @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+            @cart_item.amount += params[:cart_item][:amount].to_i
+            @cart_item.update(amount: cart_item.amount)
+            redirect_to cart_items_path
+        else
+            @cart_item.save
+            redirect_to cart_items_path
+        end
     end
     
     private
