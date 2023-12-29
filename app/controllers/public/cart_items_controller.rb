6b7f98213f30f9a18ef9854b278782d03e3e37f6
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2023-12-29 03:18:49 +0000

    Add カートへの追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,5 +14,13 @@ class Public::CartItemsController < ApplicationController
     end
     
     def create
+        binding.pry
+        @item = Item.find(cart_item_params[:item_id])
+    end
+    
+    private
+    
+    def cart_item_params
+        params.require(:cart_item).permit(:item_id, :amount)
     end
 end
