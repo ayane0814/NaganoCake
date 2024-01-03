b7b408d8e4ad34a7853e559e224a337aae3183d8
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-01-03 08:55:51 +0000

    Add 注文情報入力作成途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,9 +2,16 @@ class Public::OrdersController < ApplicationController
     before_action :authenticate_customer!
     
     def new
+        @order = Order.new
     end
     
     def confirm
+        @order = Order.new(order_params)
+        @order.post_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.address_name = current_customer.fullname
+        @cart_items = current_customer.cart_items.all
+        @total_amount = 0
     end
     
     def thanks
@@ -17,5 +24,11 @@ class Public::OrdersController < ApplicationController
     end
     
     def show
+    end
+    
+    private
+    
+    def order_params
+        params.require(:order).permit(:payment_method)
     end
 end
