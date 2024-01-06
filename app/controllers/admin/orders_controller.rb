fa26abb6150c82a9d46ac23602cb64318b8a1e24
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-01-06 12:16:38 +0000

    Add 注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,5 +2,7 @@ class Admin::OrdersController < ApplicationController
     before_action :authenticate_admin!
     
     def show
+        @order = Order.find(params[:id])
+        @order_items = OrderItem.where(order_id: params[:id])
     end
 end
